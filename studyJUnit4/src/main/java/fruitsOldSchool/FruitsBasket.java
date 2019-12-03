25786c955140a9c6e43d397a31b4953563b2e192
igv7 <46227257+igv7@users.noreply.github.com>
2019-12-03 20:09:46 +0200

    Adding a code old school

---
+++ studyJUnit4/src/main/java/fruitsOldSchool/FruitsBasket.java
@@ -0,0 +1,29 @@
+package fruitsOldSchool;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class FruitsBasket {
+
+    private List<Fruit> basket = new ArrayList<>();
+
+    public void add(Fruit fruit) {
+    	basket.add(fruit);
+    }
+
+    public void remove(Fruit fruit) {
+        if (!basket.contains(fruit)) {
+            throw new NoSuchElementException();
+        }
+        basket.remove(fruit);
+    }
+
+    public int getSize() {
+        return basket.size();
+    }
+
+    public void removeAll() {
+    	basket.clear();
+    }
+}
